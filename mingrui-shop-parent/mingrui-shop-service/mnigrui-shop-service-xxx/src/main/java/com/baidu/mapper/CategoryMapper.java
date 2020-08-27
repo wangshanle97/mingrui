6f8dcb9916e86c8c747f7d2d71b888d44be54ba3
wangshanle <wangshanle97@163.com>
2020-08-27 21:59:27 +0800

    商品查询

---
+++ mingrui-shop-parent/mingrui-shop-service/mnigrui-shop-service-xxx/src/main/java/com/baidu/mapper/CategoryMapper.java
@@ -0,0 +1,16 @@
+package com.baidu.mapper;
+
+import com.baidu.shop.entity.CategoryEntity;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @ClassName CategoryMapper
+ * @Description: TODO
+ * @Author wangshanle
+ * @Date 2020/8/27
+ * @Version V1.0
+ **/
+
+public interface CategoryMapper extends Mapper<CategoryEntity> {
+
+}
