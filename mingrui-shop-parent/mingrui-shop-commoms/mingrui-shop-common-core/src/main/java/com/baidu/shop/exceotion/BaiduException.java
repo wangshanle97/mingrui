5eb6e67eedd3bd375cd4ed94bdc5689475923aa4
wangshanle <wangshanle97@163.com>
2020-09-16 14:04:51 +0800

    boot版本升级

--- mingrui-shop-parent/mingrui-shop-commoms/mingrui-shop-common-core/src/main/java/com/baidu/shop/exceotion/BaiduException.java
+++ mingrui-shop-parent/mingrui-shop-commoms/mingrui-shop-common-core/src/main/java/com/baidu/shop/exceotion/BaiduException.java
@@ -4,12 +4,12 @@ import com.baidu.shop.status.HTTPStatus;
 
 /**
  * @ClassName BaiduException
- * @Description: TODO
+ * DD@Description: TODO
  * @Author wangshanle
  * @Date 2020/9/3
  * @Version V1.0
  **/
-public class BaiduException extends Throwable {
+public class BaiduException extends RuntimeException {
 
     private String msg;
 
